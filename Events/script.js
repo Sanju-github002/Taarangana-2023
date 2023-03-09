64762b4b41577348c6fb9635a6592fc036386d3d
Sanju <anutanwer12@gmail.com>
2023-03-09 15:06:19 +0530

    Update script.js

--- Events/script.js
+++ Events/script.js
@@ -1,3 +1,175 @@
+const URL = "https://bits-oasis.org/registrations/events_details/";
+
+let ALL_EVENTS;
+
+const eventsNames = [
+    'Mr and Ms Taarangana',
+    'Rangmanch',
+    'Aaghaz',
+    'Rap Battle',
+    'Group Singing-Antra',
+    'Urban Thump',
+    'Solo Singing-Anhad',
+    'Lilac Dreams',
+    'Slam Poetry',
+    'Knights of the fall'
+];
+
+function fetchEvents() {
+    fetch(URL).then(resp => resp.json())
+    .then(function(response) {
+        console.log(response);
+        ALL_EVENTS = response
+    });
+}
+
+fetchEvents();
+
+const eventsImgUrl = [
+    'images/events/dance.svg',
+    'images/events/drama.svg',
+    'images/events/fine arts.svg',
+    'images/events/music.svg',
+    'images/events/photography.svg',
+    'images/events/fashion.svg',
+    'images/events/oratory.svg',
+    'images/events/quizzing.svg',
+    'images/events/humour.svg',
+    'images/events/film fest.svg',
+    'images/events/misc.svg'
+];
+
+const eventDesc = {
+    'Mr and Ms Taarangana':'Mr. and Miss Taarangana is a perfect grandstand for all the creative visionaries who can adorn their beauty and brains with grace and poise. The event helps contestants display their exuberance and assets that intoxicate both the holder and the beholder.    ',
+    'Rangmanch':'Kabhi Kabhi Kuch Jeetne Ke Liye Kuch Harna Padta Hai. Aur Haar Ke Jeetne Wale Ko Baazigar Kehte Hain.Rangmanch is a mono acting event for all the creative and prolific artists out there to showcase their unspoken thoughts or emotions through their theatrical skills. If acting is your passion and stage, light and camera are your happy place then this is your opportunity to prove yourself. Come and join us by unveiling your inner performer and exhibiting a variety of emotions and personas!',
+    'Aaghaz':'Aaghaaz is the Nukkad Natak competition to draw motley crowds of people to witness performers perform their street plays based on issues concerning society. The contestants illuminate the feast with their crisp dialogues and gestures that grasps everyone spellbound with their gripping art.',
+    'Rap Battle':'You could be the next rising star, enthralling the audience with your witty rhymes and melodious chimes! Rap Battle is an event inviting youngsters from all over the country to showcase their musicality. Prove yourself to be the best lyricist with catchy lines and hypnotise the onlookers with your hip voice.',
+    'Group Singing-Antra':'Antra is an event which acknowledges all the distinct musical identities with a voice meant to be heard. An opportunity to capture the stage, lure everyone and leave the audience spell-bound. This is the time for the contestants to show all their raags and taals. ',
+    'Urban Thump':'Western Group Dance!  Urban Thump is the ideal platform for all the dancers who want to enchant everyone with their catchy steps and mesmerising performances. The event helps the contesting teams exhibit their strengths, grace and coordination through dance moves. ',
+    'Solo Singing-Anhad':'Anhad is a spectacular stage for singers and musicians to display their sweet voices and unparalleled charms! From mellow tunes to lively numbers, this dynamic event invites all. The participants will showcase their shining vocals and compete for the audience’s vote! ',
+    'Lilac Dreams': 'An event for those who consider the ramp their safe haven!Liliac Dreams aspires to provide a platform for fashionistas to showcase their vision, creativity and emotions through fashion and art. Each team walks the ramp and puts forth a stunning performance, letting their unique creations speak for themselves. ',
+    'Slam Poetry':'Kavita Kahaniya jo dil chu le!....Slam Poetry is an event to satisfy the dreamer, wanderer, and poet in you. Gather your untold musings, settle down with a warm cup of coffee to speak your heart out, and connect with other noble souls!',
+    'Knights of the fall':'Knights of the fall provides an opportunity for aspiring and semi-professional college bands to battle it out on the stage. In this band tussle, hearts throb to the beat of the drums, and the air buzzes with musical intensity. Bring your amazing rhythms and insane riffs to dazzle the audience.'
+};
+
+const eventReg = {
+    'Mr and Ms Taarangana':'https://forms.gle/KPfK2as15LSVbBz9A',
+    'Rangmanch':'https://forms.gle/TVhRkdy61hYey7FZ8',
+    'Aaghaz':'https://forms.gle/zMqRs9Gk8qnnKwAK8',
+    'Rap Battle':'https://docs.google.com/forms/d/e/1FAIpQLScu5BEWZf6UzGFTJEI77ywX-5LTl03IBFL0HElmjnsDePEI1g/viewform?usp=sf_link',
+    'Group Singing-Antra':'https://forms.gle/3o2Yorc2i1HhnP2c8',
+    'Urban Thump':'https://forms.gle/PLtduUDHwmfcLSVPA',
+    'Solo Singing-Anhad':'https://docs.google.com/forms/d/e/1FAIpQLScd6uE5BhxzTHC39ZNJymNcFviXKpTkva_N1nxf3yG4gGz0bA/viewform?usp=sf_link',
+    'Lilac Dreams':'https://docs.google.com/forms/d/1CgYH64GaLk3CKpkPL8kt7yNPrjT1aaO7RvsVaCzpJEk/edit',
+    'Slam Poetry':'https://forms.gle/HMTvyfr7sRqvhW4P7',
+    'Knights of the fall':'https://docs.google.com/forms/d/e/1FAIpQLSd-sniJldi6kp6pL2vetAdih0WPeEJhwe12bgWhawdGyeLDHQ/viewform?usp=sf_link' 
+};
+
+
+const eventsContainer = document.getElementsByClassName("events-container")[0];
+
+const numberOfEvents = eventsNames.length;
+let eventIndex = 1;
+
+const createEvent = (parentAppend) => {
+    for (j = 0; j < numberOfEvents; j++) {
+        let eventsCard = document.createElement('div');
+        eventsCard.className = "events-card";
+        if (j == 1) {
+            eventsCard.classList.add('active');
+        }
+
+        let eventsCardFront = document.createElement('div');
+        eventsCardFront.className = "events-card-front";
+
+        let eventsNameContainer = document.createElement('div');
+        eventsNameContainer.className = "event-name";
+        let eventsName = document.createTextNode(eventsNames[j]);
+        eventsNameContainer.appendChild(eventsName);
+        eventsCardFront.appendChild(eventsNameContainer);
+
+        // let eventsCardFrontImg = document.createElement('div');
+        // eventsCardFrontImg.className = "event-img"
+        // eventsCardFrontImg.setAttribute('style', "background-image: url('" + eventsImgUrl[j] + "')");
+        // eventsCardFront.appendChild(eventsCardFrontImg);
+
+        // let seperator = document.createElement('hr');
+        // seperator.align = 'center';
+        // seperator.width = '35%';
+        // seperator.size = '5px';
+        // seperator.color = '#9A6D9A';
+        // eventsCardFront.appendChild(seperator);
+
+        let eventsCardFrontLink = document.createElement('div');
+        eventsCardFrontLink.className = "event-link";
+        eventsCardFrontLink.innerHTML = "View Details >";
+        eventsCardFront.appendChild(eventsCardFrontLink);
+
+        eventsCard.appendChild(eventsCardFront);
+
+        parentAppend.appendChild(eventsCard);
+        const eventType = eventsNames[j];
+        eventsCardFrontLink.addEventListener('click', () => {
+            openAllEvents(eventType)
+        });
+    }
+}
+
+const createEventDots = () => {
+    const dotsContainer = document.getElementsByClassName('events-nav-dots')[0];
+    for (i = 0; i < ((window.innerWidth > 600) ? (numberOfEvents - 2) : numberOfEvents); i++) {
+        let dot = document.createElement('div');
+        dot.className = "event-nav-dot";
+        dot.id = (i + 1);
+        dot.setAttribute('onclick', 'navigateEvent(this.id)');
+        if (i === 0) {
+            dot.classList.add('active-dot');
+        }
+        dotsContainer.appendChild(dot);
+    }
+}
+
+createEventDots();
+createEvent(eventsContainer);
+createEvent(document.getElementsByClassName('all-events-type-container')[0]);
+
+
+
+
+const changeEventSet = () => {
+    const limit = numberOfEvents;
+    if (eventIndex < limit) {
+        eventIndex++;
+        if (window.innerWidth > 600) {
+            eventsContainer.style.transform = "translate(" + (-27.15 * (eventIndex - 1)) + "%)";
+        } else {
+            eventsContainer.style.transform = "translate(" + (-100 * (eventIndex - 1)) + "%)";
+        }
+        (eventIndex == numberOfEvents) ? null : document.getElementsByClassName('events-card')[eventIndex].classList.add('active');
+        for (dot of document.getElementsByClassName('event-nav-dot')) {
+            dot.classList.remove('active-dot');
+        }
+        document.querySelectorAll('.events-nav-dots > .event-nav-dot')[eventIndex - 1].classList.add('active-dot')
+    } else {
+        eventIndex = 0;
+        changeEventSet();
+    }
+}
+
+const navigateEvent = (dotIndex) => {
+    if(dotIndex <= 0) {
+        eventIndex = ((window.innerWidth > 600) ? (numberOfEvents - 3) : (numberOfEvents - 1));;
+        changeEventSet();
+        return;
+    }
+    if(dotIndex > ((window.innerWidth > 600) ? (numberOfEvents - 2) : numberOfEvents)) {
+        eventIndex = 0;
+        changeEventSet();
+        return;
+    }
+    if (eventIndex != dotIndex) {
+        (eventIndex == numberOfEvents) ? null : document.getElementsByClassName('events-card')[eventIndex].classList.remove('active');
+        eventIndex = dotIndex - 1;
         changeEventSet();
     }
 }
